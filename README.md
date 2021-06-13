91aa88dacd3e9641a4832416339770a61288cddf
Eric Bourne <ericbourne6371@gmail.com>
2021-06-12 20:59:18 -0700

    newData

---
+++ README.md
@@ -0,0 +1,5 @@
+# Eric Bourne's Portfolio Source Code
+
+## Welcome to my portfolio!
+
+This site was built using the gatsby JS framework. This is the source code for my website, a showcase of my favourite projects.
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import App from '../components/App';
+import { headData } from '../mock/data';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/main.scss';
+//
+export default () => {
+  const { title, lang, description, ogImg } = headData;
+
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <html lang={lang} />
+        <meta name="description" content={description} />
+        <meta name="image" property="og:image" content={ogImg} />
+      </Helmet>
+      <App />
+    </>
+  );
+};
